72621a881a8a24d82bea8899e665de859f3af02a
hseiji <henrique.hst@gmail.com>
2022-02-28 20:39:49 -0500

    save new url to urlDatabase and redirect

--- express_server.js
+++ express_server.js
@@ -40,14 +40,23 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+  const tinyUrl = generateRandomString();
+  urlDatabase[tinyUrl] = req.body.longURL;
+  res.redirect(`/urls/${tinyUrl}`);
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  // console.log(urlDatabase);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
